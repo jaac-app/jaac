08e1dd3bd601753ce75f38854e07284bee2989d5
Amandeep-Bhullar <er.amandeepbhullar@gmail.com>
2020-07-18 12:12:41 -0400

    fixed port for Heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const port = 3000 || process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.use('/', express.static(path.join(__dirname, '/public')))
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')))
