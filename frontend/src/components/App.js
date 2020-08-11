80c22cc8ea0d53dc2c7a5bcc2b471dc491caa8db
Amandeep-Bhullar <er.amandeepbhullar@gmail.com>
2020-08-10 20:24:23 -0400

    tried to fixed aurnab message

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,11 +2,12 @@ import React from 'react'
 
 import 'css/App.css'
 
-import Homepage from "../pages/homepage/Homepage";
+// import Homepage from "../pages/homepage/Homepage";
+import Layout from "./Layout";
 
 const App = () => {
 	return (
-		<Homepage />
+		<Layout />
 	)
 }
 
